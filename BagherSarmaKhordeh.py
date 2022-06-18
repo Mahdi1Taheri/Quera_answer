5cef2947803d97aa322252b044d69ce60493cadd
Roham2021 <96267873+Roham2021@users.noreply.github.com>
2022-06-18 15:41:14 +0430

    Update BagherSarmaKhordeh.py

--- BagherSarmaKhordeh.py
+++ BagherSarmaKhordeh.py
@@ -1,7 +1,4 @@
 
-
-
-
 strings = []  
 count = ""
   
@@ -9,30 +6,8 @@ for i in range(1,6):
     Recevie = input().upper()[:20]
     if 'MOLANA' in Recevie or 'HAFEZ' in Recevie:   
             strings.append(i)
-            # count += str(f'{i} ')
-
-          
+       
 if strings == []:
     print("NOT FOUND!")
 
 print(*strings)
-
-
-
-
-# for i in range(1,6):
-#     Recevie = input("please enter the strings(capital letters, numbers, char(-)) %s: " % (int(i))).upper()
-#     strings.append(Recevie)
-
-# for j in range(len(strings)):
-#     if 'MOLANA' in strings[j] or 'HAFEZ' in strings[j]:
-#         print(j)
-#     if 'MOLANA' not in strings[j] or 'HAFEZ' not in strings[j]:
-#         print("NOT FOUND!")
-
-
-    
-    
-    
-
-
